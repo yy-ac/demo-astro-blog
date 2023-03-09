d0658b081df93cb0f867989899edcbb054c514c6
Soltus <liaoshanyi@qq.com>
2023-03-09 22:48:41 +0800

    no message

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yy-ac.github.io',
+  base: '/demo-astro-blog',
 	integrations: [mdx(), sitemap()],
 });
